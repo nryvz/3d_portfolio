f818cf52e949e01a373d1745516af5b23abd5940
Nuriye Yavuz <nuriyeyavuz89@gmail.com>
2025-03-18 14:00:22 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import {
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/3d_portfolio">
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bd-cover bg-no-repeat bg-center">
           <Navbar />
